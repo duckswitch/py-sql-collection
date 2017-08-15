8c206e5ed4f5fa571b1a52509f07d06ed533dc60
Kévin LAMBERT <kevin.lambert@free.fr>
2017-08-15 23:33:52 +0200

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+Setup file describing how to install.
+"""
+
+from setuptools import setup
+
+setup(
+    name=u'py-sql-collection',
+    version=u'0.1',
+    packages=[u'py-sql-collection'],
+    install_requires=[u'MySQL-python',],
+    url='https://github.com/duckswitch/py-sql-collection.git',
+    download_url='https://github.com/duckswitch/py-sql-collection.git/archive/0.1.tar.gz',
+    keywords=[]
+)
