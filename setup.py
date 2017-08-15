6cc79991bc8a8aca25c2da1232421f256d6ffbb9
Kévin LAMBERT <kevin.lambert@free.fr>
2017-08-15 23:40:26 +0200

    Correct typo

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from setuptools import setup
 setup(
     name=u'py-sql-collection',
     version=u'0.1',
-    packages=[u'pysqlcollection', u'pysqlcollection.serializer', u'pysqlcollection/connection'],
-    install_requires=[u'MySQL-python',],
+    packages=[u'pysqlcollection', u'pysqlcollection.serializer', u'pysqlcollection.connection'],
+    install_requires=[u'MySQL-python'],
     url='https://github.com/duckswitch/py-sql-collection.git',
     download_url='https://github.com/duckswitch/py-sql-collection.git/archive/0.1.tar.gz',
     keywords=[]
