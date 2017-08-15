1925405a0fa6909917a97c1895d8a537be7bff15
Kévin LAMBERT <kevin.lambert@free.fr>
2017-08-15 23:35:47 +0200

    Correct setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 setup(
     name=u'py-sql-collection',
     version=u'0.1',
-    packages=[u'py-sql-collection'],
+    packages=[u'pysqlcollection'],
     install_requires=[u'MySQL-python',],
     url='https://github.com/duckswitch/py-sql-collection.git',
     download_url='https://github.com/duckswitch/py-sql-collection.git/archive/0.1.tar.gz',
