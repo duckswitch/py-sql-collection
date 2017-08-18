1baf4f6e9d7d2d3f0461af7a9871c677b44dc9b2
Kévin LAMBERT <kevin.lambert@free.fr>
2017-08-18 23:01:04 +0200

    Update helper with timestamp

--- helper.py
+++ helper.py
@@ -8,10 +8,13 @@ client = Client(host=u"127.0.0.1", user=u"root", password=u"localroot1234")
 
 hours_count = client.hours_count
 description = hours_count.hour.get_description(auto_lookup=0)
-print(json.dumps(description, indent=4))
 
 cursor = hours_count.hour.find(
-    query={},
+    query={
+        u"started_at": {
+            u"$lt": 1503088575
+        }
+    },
     lookup=[
         {
             u"from": u"project",
@@ -20,25 +23,26 @@ cursor = hours_count.hour.find(
         }
     ]
 ).limit(1).skip(0).sort([(u"id", 1)])
-
+#
 for item in cursor:
     print(json.dumps(item, indent=4))
-
-cursor = hours_count.hour.find(
-    projection={
-        u"project.name": 1
-    },
-    auto_lookup=2
-).limit(1).skip(0)
-
-for item in cursor:
-    print(json.dumps(item, indent=4))
-
-
-
+#
+# cursor = hours_count.hour.find(
+#     projection={
+#         u"project.name": 1
+#     },
+#     auto_lookup=2
+# ).limit(1).skip(0)
+#
+# for item in cursor:
+#     print(json.dumps(item, indent=4))
+#
+#
+#
+#
 
 result = hours_count.hour.insert_one({
-    u"started_at": u"2017-05-01",
+    u"started_at": 1503088575,
     u"minutes": 15,
     u"project": {
         u"id": 4
@@ -46,14 +50,12 @@ result = hours_count.hour.insert_one({
     u"affected_to": 1
 }, auto_lookup=2)
 
-
-print(result.inserted_id)
-
-result = hours_count.project.update_many(query={
-    u"banane.name": u"TEST 2"
-}, update={
+#
+# print(result.inserted_id)
+#
+result = hours_count.project.update_many(query={}, update={
     u"$set": {
-        u"name": u"PATATE"
+        u"started_at": 1503088575
     }
 }, options={
     u"upsert": False,
@@ -67,17 +69,17 @@ result = hours_count.project.update_many(query={
     }
 ])
 
-
 #
-result = hours_count.project.delete_many(query={
-    u"banane.name": u"TEST 2"
-}, lookup=[
-    {
-        u"from": u"client",
-        u"localField": u"client",
-        u"foreignField": u"id",
-        u"as": u"banane"
-    }
-])
-#
-print(result.deleted_count)
+# #
+# result = hours_count.project.delete_many(query={
+#     u"banane.name": u"TEST 2"
+# }, lookup=[
+#     {
+#         u"from": u"client",
+#         u"localField": u"client",
+#         u"foreignField": u"id",
+#         u"as": u"banane"
+#     }
+# ])
+# #
+# print(result.deleted_count)
