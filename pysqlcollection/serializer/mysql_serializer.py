a5a2594b85fe0c2c91e5be08384fa00d60feb2b9
Kévin LAMBERT <kevin.lambert@free.fr>
2017-09-03 14:48:17 +0200

    Correct limit & offset bug.

--- pysqlcollection/serializer/mysql_serializer.py
+++ pysqlcollection/serializer/mysql_serializer.py
@@ -109,17 +109,17 @@ class MySQLSerializer(AbstractSQLSerializer):
             -1: u"DESC"
         }
 
-        limit_offset = u""
+         limit_offset = u""
         if with_limit_and_skip:
             limit_offset = u"LIMIT %s OFFSET %s"
             values += [select.limit, select.offset]
 
         sorts = u", ".join([u"`{}` {}".format(sort.field.alias, sort_bindings[sort.direction]) for sort in select.sorts])
         sorts = u"ORDER BY {}".format(sorts) if len(sorts) > 0 else sorts
-        query = u"SELECT {} FROM (SELECT * FROM {} {}) {} {} ".format(u", ".join(fields), select.table.name, limit_offset, select.table.alias, joins)
+        query = u"SELECT {} FROM (SELECT * FROM {}) {} {} ".format(u", ".join(fields), select.table.name, select.table.alias, joins)
 
         displayed = u", ".join([u"`{}`".format(field.alias) for field in select.fields if field.display])
-        query = u"SELECT {} FROM ({}) AS A0 {} {}".format(displayed, query, where, sorts)
+        query = u"SELECT {} FROM ({}) AS A0 {} {} {}".format(displayed, query, where, sorts, limit_offset)
         return query, values
 
     def get_relations(self, database_name, table_name):
